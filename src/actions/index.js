71643af1230744a1d75d470db8b26f0f42c79a73
Oleh Lytvyn <oleh.lytvyn@gmail.com>
2019-07-15 19:37:06 +0300

    Add balance

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,18 +1,5 @@
-/*
- * action types
- */
-
 export const ADD_TRASACTION = 'ADD_TODO';
 export const SET_SUCCESS_ADD = 'SET_SUCCESS_ADD';
-
-/*
- * other constants
- */
-
- 
-/*
- * action creators
- */
 
 export const addTransaction = (transaction) => {
   return { type: ADD_TRASACTION, transaction }
